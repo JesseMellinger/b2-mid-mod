d7e51df42b381bb48ddd47d9db550861dc2e1440
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-23 10:28:18 -0600

    Create feature test for mechanics index page

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "as a visitor" do
+  before :each do
+    @mechanic_1 = Mechanic.create!(name: "Geordi La Forge",
+                                  years_of_experience: 25)
+
+    @mechanic_2 = Mechanic.create!(name: "Fonzie ",
+                                  years_of_experience: 20)
+  end
+  describe "when I visit a mechanics index page" do
+    it "I see a header saying 'All Mechanics' and I see a list of all mechanic’s names and their years of experience" do
+
+      visit("/mechanics")
+
+      expect(page).to have_content("All Mechanics")
+
+      within("#mechanic-#{@mechanic_1.id}") do
+        expect(page).to have_content("#{@mechanic_1.name} - #{@mechanic_1.years_of_experience} years of experience")
+      end
+
+      within("#mechanic-#{@mechanic_2.id}") do
+        expect(page).to have_content("#{@mechanic_2.name} - #{@mechanic_2.years_of_experience} years of experience")
+      end
+    end
+  end
+end
